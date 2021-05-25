d7bb67330d73cb781ebcf1e5a3998b89a110c4fb
Shivam Sharma <the.alpha.tronixm@gmail.com>
2021-05-25 18:05:46 +0530

    Added: Routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React from 'react';
 import './App.css';
 import Header from './component/header'
 import Home from './component/homepage'
+import Detail from './component/Detail'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 function App() {
   return (
     <div className="App">
+      <Router>
       <Header />
-      <Home />
+      <Switch>
+        <Router path="/detail">
+          <Detail />
+        </Router>
+        <Route path="/">
+          <Home />
+          </Route>
+
+      </Switch>
+      </Router>
+      
     </div>
   );
 }
 
 export default App;
+
+//Every Page shall have the Header/Nav. 
+//Body remians same.
